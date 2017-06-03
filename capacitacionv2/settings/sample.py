a127288d82c6e2fe7ed9c4790ec53fa8a1a4db7b
Lucho <lucho.farfan9@gmail.com>
2017-06-03 16:45:23 -0500

    last changes

--- capacitacionv2/settings/sample.py
+++ capacitacionv2/settings/sample.py
@@ -1,11 +1,3 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'izzsuvka)mnc)c_u53=o319(#(%jd)hs#^#ua1bawogi21b2@+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
 DATABASES = {
     # BASE DE DATOS PRINCIPAL PARA CAPACITACIÓN
     'default': {
@@ -46,13 +38,3 @@ DATABASES = {
         },
     }
 }
-
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = (
-#     '192.168.200.123:8001',
-#     'cpv.inei.gob.pe:5050',
-#     'cpv.inei.gob.pe:85',
-#     '172.16.2.205:8000',
-#     'localhost:3000',
-#     '192.168.200.123:3000',
-# )
