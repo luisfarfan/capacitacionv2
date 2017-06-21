02a1e3e7dd6cd4fcfae2265e3666eab53118632c
alexander490 <alexander4_90@hotmail.com>
2017-06-21 11:21:39 -0500

    2 decimls

--- static/src/sininternet/sininternet.view.ts
+++ static/src/sininternet/sininternet.view.ts
@@ -195,23 +195,30 @@ class SinInternetView {
         console.log(this.personalNoInternet);
         let disabled:string = '';
         this.personalNoInternet.map((peanota: IPeaNotaFinalSinInternet, index: number) => {
-            let nota_fina_p:string;
+
             if (peanota.nota_final == null){
                 disabled = '';
-                nota_fina_p=(peanota.nota_final).toString();
-            }
-            else{
-                nota_fina_p=(peanota.nota_final).toFixed(2);
-                disabled='disabled';
-            }
-            html += `<tr data-value="${peanota.pea.id_pea}">
+                html += `<tr data-value="${peanota.pea.id_pea}">
                         <td>${index + 1}</td>
                         <td>${peanota.pea.ape_paterno} ${peanota.pea.ape_materno} ${peanota.pea.nombre}</td>
                         <td>${peanota.pea.dni}</td>
                         <td>${peanota.pea.zona}</td>
-                        <td><input ${disabled} name="nota_final" value="${nota_fina_p}" type="number"></td>
+                        <td><input ${disabled} name="nota_final" value="${peanota.nota_final}" type="number" step="0.01"></td>
                       
                      </tr>`;
+            }
+            else{
+                disabled='disabled';
+                html += `<tr data-value="${peanota.pea.id_pea}">
+                        <td>${index + 1}</td>
+                        <td>${peanota.pea.ape_paterno} ${peanota.pea.ape_materno} ${peanota.pea.nombre}</td>
+                        <td>${peanota.pea.dni}</td>
+                        <td>${peanota.pea.zona}</td>
+                        <td><input ${disabled} name="nota_final" value="${peanota.nota_final.toFixed(2)}" type="number" step="0.01"></td>
+                      
+                     </tr>`;
+            }
+
         });
         $('#table_personalnotafinal1').find('tbody').html(html);
     }
